9743bf50d3badc2db2a65c3f9e2bcbe98db57c26
Mario <mario@macbook-mario.local>
2019-06-07 16:06:03 -0300

    refactor main to use new translate-markup syntax

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 // tslint:disable-next-line: no-implicit-dependencies
-import { Engine, Translator } from 'translate-markup';
+import { getJSTranslation } from 'translate-markup';
 
 export default async function(source: string) {
-  const translator = new Translator();
-  const engine = new Engine();
-
-  const translationsArray = await translator.generateYamlFileTranslationsArray({
-    fileContent: source
-  });
-
-  const translationContent = engine.getJSTranslationsString({
-    fileTranslations: translationsArray
+  const translationContent = await getJSTranslation({
+    yamlLangContent: source
   });
 
   return translationContent;
